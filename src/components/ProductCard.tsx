da9a5fb74f311972166a3a9f5da8780258c19f19
Aarogya <aarufloyd@gmail.com>
2026-02-03 18:03:11 +0545

    Add to cart is functionable from every path

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,25 +1,28 @@
 import type { ProductType } from "../data/data";
 import styles from "../Styles/ProductCard.module.css";
-const ProductCard=({product,
+
+const ProductCard = ({
+  product,
   AddtoCart,
-  showButton=true,
-  classname=""
-}:{product:ProductType
-  AddtoCart:(product:ProductType)=>void;
-  showButton?:boolean;
-  classname?:string;
-})=>{
+  showButton = true,
+  classname = ""
+}: {
+  product: ProductType;
+  AddtoCart?: (product: ProductType) => void;   // ✅ optional single product
+  showButton?: boolean;
+  classname?: string;
+}) => {
   return (
-    <div className={`${styles.card}${classname}`}>
+    <div className={`${styles.card} ${classname}`}>
       <img src={product.image} alt={product.name} className={styles.image} />
       <h3>{product.name}</h3>
-      <h3>{` Item Categories :${product.category}`}</h3>
+      <h3>{`Item Category: ${product.category}`}</h3>
       <p>{product.brand}</p>
       <p>${product.price}</p>
       {showButton && AddtoCart && (
-      <button onClick={()=>AddtoCart(product)} >
-        🛒 Add to cart
-      </button>
+        <button onClick={() => AddtoCart(product)}>
+          🛒 Add to cart
+        </button>
       )}
     </div>
   );
